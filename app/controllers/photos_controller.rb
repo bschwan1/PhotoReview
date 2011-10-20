87f0b8176ac075e2ded547320953bd7dcbd2f886
Wm Schwan <bschwan1@lycos.com>
2011-10-20 09:35:41 -0600

    Modified Photos controller for Heroku error

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,9 +4,9 @@ class PhotosController < ApplicationController
   def code_image
     @image_data = Photo.find(params[:id])
     @image = @image_data.binary_data
-    send_data (@image, :type => @image_data.content_type,
-                       :filename => @image_data.filename,
-                       :disposition => 'inline')
+    send_data @image, :type => @image_data.content_type,
+                      :filename => @image_data.filename,
+                      :disposition => 'inline'
   end
 
   # GET /photos
