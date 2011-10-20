19f84049ff8618c6a5148cade2534a185bfde2d6
Wm Schwan <bschwan1@lycos.com>
2011-10-20 14:14:18 -0600

    Compiled assets locally for Heroku and unit test cases

--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PhotoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without Title Filename and Content Type" do
+    photo = Photo.new
+    assert !photo.save
+  end
+  test "should not save with title less than six chars" do
+    photo = Photo.new
+    photo.title = "Title"
+    photo.filename = "testFile.jpg"
+    photo.content_type = "image/jpeg"
+    assert !photo.save
+  end
+  test "should not save non jpeg file" do
+    photo = Photo.new
+    photo.title = "Title One"
+    photo.filename = "testFile.gif"
+    photo.content_type = "image/gif"
+    assert !photo.save
+  end
+  test "should save this photo with Title One" do
+    photo = Photo.new
+    photo.title = "Title One"
+    photo.filename = "testFile.jpg"
+    photo.content_type = "image/jpeg"
+    assert photo.save
+  end
 end
--- test/unit/review_test.rb
+++ test/unit/review_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "no save without reviewer body" do
+    review = Review.new
+    assert !review.save
+  end
+  test "should not save with reviewer less three chars" do
+    review = Review.new
+    review.reviewer = "WN"
+    review.body = "This is the body of the review."
+    assert !review.save
+  end
+  test "should not save with nil body" do
+    review = Review.new
+    review.reviewer = "WNS"
+    review.body = nil
+    assert !review.save
+  end
+  test "should save reviewer with reviewer body" do
+    review = Review.new
+    review.reviewer = "WNS"
+    review.body = "This is the body of the review."
+    assert review.save
+  end
+
 end
